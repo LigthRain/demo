38e44b9eb05766830c6e3b75728e8b836cf002f5
anzihan <13991821330@163.com>
2020-02-18 17:07:25 +0800

    test

---
+++ src/main/java/JVMBase/InitTest.java
@@ -0,0 +1,16 @@
+package JVMBase;
+
+public class InitTest {
+
+    public int a;
+//    public final int c;
+
+    public static void main(String[] args) {
+        int b;
+        InitTest initTest = new InitTest();
+        System.out.println(initTest.a);
+//        initTest.c = 4;
+//        System.out.println(b);
+    }
+
+}
